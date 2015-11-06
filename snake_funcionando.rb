8338f561913811e5db0492a24fbafefccc3574e0
Pedro Silva <pedrro2@hotmail.com>
2015-11-06 10:58:43 -0200

    @timeAceleradora - refatorando snake

--- snake_funcionando.rb
+++ snake_funcionando.rb
@@ -7,126 +7,122 @@ Funcionalidades faltantes:
   X não está aumentando de tamanho quando pega comida
 =end
 #INICIANDO A MATRIZ
-matriz = Array.new
-matriz = [[],[],[],[],[],[],[]]
-15.times{matriz[0] << "-"}
-15.times{matriz[1] << "-"}
-15.times{matriz[2] << "-"}
-15.times{matriz[3] << "-"}
-15.times{matriz[4] << "-"}
-15.times{matriz[5] << "-"}
-15.times{matriz[6] << "-"}
-@V = true
-@lin = 0
-@con = 0
-matriz[0][0]= "X"
-@x = rand(14)
-@y = rand(6)
-@x=@x.to_i
-@x=@y.to_i
-matriz[@x][@y] = "o"
+matriz = Array.new(7, '-') { Array.new 15, '-' }
+
+@flag = true
+@eixo_x_da_cobra = 0
+@eixo_y_da_cobra = 0
+matriz[0][0] = "X"
+@eixo_x_da_comida = rand(14)
+@eixo_y_da_comida = rand(6)
+@eixo_x_da_comida = @eixo_x_da_comida.to_i
+@eixo_x_da_comida = @eixo_y_da_comida.to_i
+matriz[@eixo_x_da_comida][@eixo_y_da_comida] = "o"
 @comida = 0
+
 ###############################
 #Metodo de impressao
-def imprimir (mat,com)
-system 'clear'#LIMPA A TELA
-puts"Bem Vindo ao jogo da Cobrinha!"
-puts "Para mover digite A,W,D ou S e Q para sair"
-puts" "
-print "  "
-mat[0].each {|l|print "#{l}"}
-puts " "
-print "  "
-mat[1].each {|l|print "#{l}"}
-puts " "
-print "  "
-mat[2].each {|l|print "#{l}"}
-puts " "
-print "  "
-mat[3].each {|l|print "#{l}"}
-puts " "
-print "  "
-mat[4].each {|l|print "#{l}"}
-puts " "
-print "  "
-mat[5].each {|l|print "#{l}"}
-puts " "
-print "  "
-mat[6].each {|l|print "#{l}"}
-puts " "
-puts "Comida: #{com}"
+def imprimir (matriz, comida)
+  system 'clear'
+  puts"Bem Vindo ao jogo da Cobrinha!"
+  puts "Para mover digite A,W,D ou S e Q para sair"
+
+  (0..6).each do |linha|
+    puts " "
+    print "  "
+    matriz[linha].each { |coluna| print "#{coluna}" }
+  end
+
+  puts " "
+  puts "Comida: #{comida}"
 end
-###############################
-#MOVIMENTO
-def mover(mat,letra,lin,con)
-  #Mover para baixo
-  if letra =="s"&&lin<7
-    mat[lin][con]="-"
-    mat[lin+1][con]="X"
-    lin += 1
-    @lin = lin
-  end
-  #Mover para cima
-  if letra =="w"&&lin>-1
-    mat[lin][con]="-"
-    mat[lin-1][con]="X"
-    lin -=1
-    @lin=lin
-  end
-  #Mover para direita
-  if letra =="d"&&con<15
-    mat[lin][con]="-"
-    mat[lin][con+1]="X"
-    con +=1
-    @con=con
-  end
-  #Mover para esquerda
-  if letra =="a"&&con>-1
-    mat[lin][con]="-"
-    mat[lin][con-1]="X"
-    con -=1
-    @con=con
+
+def mover(matriz, letra, linha, coluna)
+  matriz[linha][coluna] = "-"
+  case letra.downcase
+    when 's'
+      mover_para_baixo(matriz, linha, coluna)
+    when 'w'
+      mover_para_cima(matriz, linha, coluna)
+    when 'd'
+      mover_para_a_direita(matriz, linha, coluna)
+    when 'a'
+      mover_para_a_esquerda(matriz, linha, coluna)
   end
 end
+
+def mover_para_baixo(matriz, linha, coluna)
+  if linha < 7
+    matriz[linha + 1][coluna] = "X"
+    linha += 1
+    @eixo_x_da_cobra = linha
+  end
+end
+
+def mover_para_cima(matriz, linha, coluna)
+  if linha > -1
+    matriz[linha - 1][coluna] = "X"
+    linha -= 1
+    @eixo_x_da_cobra = linha
+  end
+end
+
+def mover_para_a_direita(matriz, linha, coluna)
+  if coluna < 15
+    matriz[linha][coluna + 1] = "X"
+    coluna += 1
+    @eixo_y_da_cobra = coluna
+  end
+end
+
+def mover_para_a_esquerda(matriz, linha, coluna)
+  if coluna > -1
+    matriz[linha][coluna - 1] = "X"
+    coluna -= 1
+    @eixo_y_da_cobra = coluna
+  end
+end
+
 ###############################
 #SAIDA
-def sair (l)
-  if l == "q" || l == "Q"
-    @V=false
-  end
+def sair (letra)
+  @flag = false if letra.downcase.eql? 'q'
+  # if letra == "q" || letra == "Q"
+  #   @flag = false
+  # end
 end
 ###############################
 #GERAR COMIDA
 def gera_comida(mat)
-  if @x==@lin && @y ==@con
-   @x=rand(14)#linha
-   @y=rand(6)#coluna
-   @x=@x.to_i
-   @x=@y.to_i
-   while @x==@lin && @y==@con#para n gerar comida em cima do X
-     @x=rand(14)#linha
-     @y=rand(6)#coluna
-     @x=@x.to_i
-     @y=@y.to_i
+  if @eixo_x_da_comida==@eixo_x_da_cobra && @eixo_y_da_comida ==@eixo_y_da_cobra
+   @eixo_x_da_comida=rand(14)#linha
+   @eixo_y_da_comida=rand(6)#coluna
+   @eixo_x_da_comida=@eixo_x_da_comida.to_i
+   @eixo_x_da_comida=@eixo_y_da_comida.to_i
+   while @eixo_x_da_comida==@eixo_x_da_cobra && @eixo_y_da_comida==@eixo_y_da_cobra#para n gerar comida em cima do X
+     @eixo_x_da_comida=rand(14)#linha
+     @eixo_y_da_comida=rand(6)#coluna
+     @eixo_x_da_comida=@eixo_x_da_comida.to_i
+     @eixo_y_da_comida=@eixo_y_da_comida.to_i
    end
-   mat[@x][@y] = "o"
+   mat[@eixo_x_da_comida][@eixo_y_da_comida] = "o"
    @comida+=1
   end
 end
 ###############################
 #PERDER NA PAREDE
 def parede()
-  if @lin<0||@con<0||@con>14
+  if @eixo_x_da_cobra<0||@eixo_y_da_cobra<0||@eixo_y_da_cobra>14
     puts "Voce Perdeu!"
-    @V=false
+    @flag = false
   end
 end
 ###############################
 #INICIANDO METODOS
-while(@V)
-    imprimir(matriz,@comida)
+while(@flag)
+    imprimir(matriz, @comida)
     letra = STDIN.getch.chomp
-    mover(matriz,letra,@lin,@con)
+    mover(matriz,letra, @eixo_x_da_cobra, @eixo_y_da_cobra)
     sair(letra)
     gera_comida(matriz)
     parede()
